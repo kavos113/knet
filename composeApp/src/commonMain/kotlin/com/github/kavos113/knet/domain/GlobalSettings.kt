e35a5b7ada34163eea4c11e3658dd27d09917f61
kavos113 <kohei.hadano@gmail.com>
2025-12-06 18:52:42 +0900

    settings

---
+++ composeApp/src/commonMain/kotlin/com/github/kavos113/knet/domain/GlobalSettings.kt
@@ -0,0 +1,7 @@
+package com.github.kavos113.knet.domain
+
+import okio.Path
+
+data class GlobalSettings(
+    val lastOpenedPath: Path?
+)
---
+++ composeApp/src/commonMain/kotlin/com/github/kavos113/knet/domain/ProjectSettings.kt
@@ -0,0 +1,15 @@
+package com.github.kavos113.knet.domain
+
+const val PROJECT_SETTINGS_DIR = ".knet"
+const val PROJECT_SETTINGS_FILE = "settings.json"
+
+data class ProjectSettings(
+    val settings: FontSettings
+)
+
+data class FontSettings(
+    val editorFontFamily: String,
+    val editorFontSize: Int,
+    val previewFontFamily: String,
+    val previewFontSize: Int,
+)
