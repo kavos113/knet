ddf4f19ae86ae3b330e940e1fa92804fbf8cade1
kavos113 <kohei.hadano@gmail.com>
2025-12-11 17:39:54 +0900

    dir item

--- composeApp/src/commonMain/kotlin/com/github/kavos113/knet/ui/explorer/ExplorerScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/kavos113/knet/ui/explorer/ExplorerScreen.kt
@@ -1,2 +1,50 @@
 package com.github.kavos113.knet.ui.explorer
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.github.kavos113.knet.ui.CommonColor
+import com.github.kavos113.knet.ui.CommonStyle
+import knet.composeapp.generated.resources.Res
+import knet.composeapp.generated.resources.chevron_right_24px
+import knet.composeapp.generated.resources.folder_24px
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DirectoryItem(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.chevron_right_24px),
+            contentDescription = "開く",
+            tint = CommonColor.foreground
+        )
+        Icon(
+            painter = painterResource(Res.drawable.folder_24px),
+            contentDescription = null,
+            tint = CommonColor.folder
+        )
+        Text(
+            text = "closed directory",
+            style = CommonStyle.normalTextStyle
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DirectoryItemPreview() {
+    DirectoryItem(modifier = Modifier.height(12.dp))
+}
--- composeApp/src/commonMain/kotlin/com/github/kavos113/knet/ui/style.kt
+++ composeApp/src/commonMain/kotlin/com/github/kavos113/knet/ui/style.kt
@@ -1,8 +1,11 @@
 package com.github.kavos113.knet.ui
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
-object BaseColor {
+object CommonColor {
     val background = Color(0xfffbfbfb)
     val backgroundDark = Color(0xffebebeb)
     val foreground = Color(0xff414141)
@@ -10,4 +13,12 @@ object BaseColor {
     val mainLight = Color(0xff91e451)
     val folder = Color(0xffe4ac10)
     val gray = Color(0xffc9c9c9)
+}
+
+object CommonStyle {
+    val normalTextStyle = TextStyle(
+        fontSize = 10.sp,
+        fontWeight = FontWeight(400),
+        color = CommonColor.foreground
+    )
 }
