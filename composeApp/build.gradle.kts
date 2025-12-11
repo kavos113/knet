f1ee8d6726f5f8e278b5d9831551dcbba73c6404
kavos113 <kohei.hadano@gmail.com>
2025-12-11 16:45:21 +0900

    color

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,6 +32,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.compose.material.icons)
             implementation(libs.okio)
         }
         commonTest.dependencies {
---
+++ composeApp/src/commonMain/kotlin/com/github/kavos113/knet/ui/color.kt
@@ -0,0 +1,13 @@
+package com.github.kavos113.knet.ui
+
+import androidx.compose.ui.graphics.Color
+
+object BaseColor {
+    val background = Color(0xfffbfbfb)
+    val backgroundDark = Color(0xffebebeb)
+    val foreground = Color(0xff414141)
+    val mainDark = Color(0xff00b400)
+    val mainLight = Color(0xff91e451)
+    val folder = Color(0xffe4ac10)
+    val gray = Color(0xffc9c9c9)
+}
